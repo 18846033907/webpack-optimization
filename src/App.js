8306496c60dea1018fd045a4e3fc1d9a1ce9a334
wenkang.yang <wenkang.yang@deltaww.com>
2021-11-29 16:57:52 +0800

    test

--- src/App.js
+++ src/App.js
@@ -6,25 +6,26 @@ import "./App.css";
 // import Index from './cloneElement'
 // import Index from './createContext'
 // import Index from './hooks'
+import Index from './hoc'
 // import {cube} from './math'
 // main.js
-import funcA from "./example1";
-import { funcG, funcH } from "./example4";
-import { funcB } from "./example2";
-import(/* webpackChunkName: "example.3" */ "./example3").then((module) => {
-  console.log("123");
-});
+// import funcA from "./example1";
+// import { funcG, funcH } from "./example4";
+// import { funcB } from "./example2";
+// import(/* webpackChunkName: "example.3" */ "./example3").then((module) => {
+//   console.log("123");
+// });
 
 const App = () => {
-  funcB();
+  // funcB();
   return (
     <div>
-      {/* <Index /> */}
-      <img src="assets/ii.jpg" />
+      <Index />
+      {/* <img src="assets/ii.jpg" />
       <img src="assets/8big.jpg" />
       <div className="parent">
         <div className="child">哈哈哈</div>
-      </div>
+      </div> */}
     </div>
   );
 };
